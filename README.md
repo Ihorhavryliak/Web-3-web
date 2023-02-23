86d19d8811eb3fb69e1e60fa14e6bbaecddb3d25
ihor <kachkamala@gmail.com>
2023-02-23 19:38:10 +0200

    change file Readme

--- README.md
+++ README.md
@@ -1,46 +1,38 @@
-# Getting Started with Create React App
+# Simple web server with a GET endpoint
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Front-end
+## Getting Started:
+`Recommended using the operating system Windows.`
+To get started with the Message App, follow these steps:
+1. Clone the repository
+2. In the terminal enter command `npm i`
 
-## Available Scripts
+## Create file `.env` with variables:
 
-In the project directory, you can run:
+`REACT_APP_URL_SERVER = http://localhost:5000/`
 
-### `npm start`
+## Run
+1. Run  by running `npm run dev` in the project directory 
+2. Go to Front-end repo:  [Back-end repo](https://github.com/Ihorhavryliak/web-3-test). (If you do not clone and install Back-end repo)
+#
+# Features
+Simple web server with a GET endpoint
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Fetches balances of all ERC20 tokens and the balance of Ethereum native tokens for a given address
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+Uses CoinGecko API to fetch token addresses
 
-### `npm test`
+Writes fetched balances to a JSON file with the latest balance and the time it was fetched
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Job works with a 1-minute interval
 
-### `npm run build`
+# Technologies Used:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Node.js
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+TypeScript
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+web3.js API (https://github.com/web3/web3.js)
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+type SetDataType = {
+  totalSupply: number;
+  tokenCount: number;
+  balance: number;
+  allTokens: [];
+};
 
 function App() {
+  const [data, setData] = useState<SetDataType[]>([]);
+  console.log(data);
+  const [secondData, setSecondData] = useState<SetDataType[]>([]);
+  const [textInput, setTextInput] = useState("");
+  const [error, setError] = useState("");
+  //download
+  const [download, setDownload] = useState(false);
+  const [downloadAllTokens, setDownloadAllTokens] = useState(false);
+  const onSend = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    setData([]);
+    setDownload(true);
+
+    //request
+    debugger;
+    axios
+      .get<SetDataType>(`${process.env.REACT_APP_URL_SERVER}total/${textInput}`)
+      .then((res: any) => {
+        setData([res.data]);
+        console.log(res.data);
+        setDownload(false);
+      })
+      .catch((error) => {
+        setError(JSON.stringify(error));
+        setDownload(false);
+      });
+
+    // send second request
+    setDownloadAllTokens(true);
+    axios
+      .get<SetDataType>(
+        `${process.env.REACT_APP_URL_SERVER}balance/${textInput}`
+      )
+      .then((res: any) => {
+        setSecondData([res.data]);
+        setDownloadAllTokens(false);
+      })
+      .catch((error) => {
+        setError(JSON.stringify(error));
+        setDownloadAllTokens(false);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row ">
+        <div className="d-flex justify-content-center">
+          <div className="col-8 ">
+            {/*   h1 */}
+            <h1 className="mt-5 mb-3">GET ERC20 tokens</h1>
+            <p className="alert alert-info">
+              GET endpoint that returns the balances of all ERC20 tokens and the
+              balance of Ethereum native tokens for a given address - what
+              tokens and how many of them are on the given address.
+            </p>
+            {/*    Warning */}
+            <p className="alert alert-warning small">
+              Warning! Might be long loading all tokens.
+            </p>
+            {/* form */}
+            <form onSubmit={onSend} className="form mb-3">
+              <input
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}
+                type="text"
+                className="form-control mb-4"
+                placeholder="Enter wallet address"
+              />
+              <button disabled={downloadAllTokens === true} className="btn btn-dark col-12">Get</button>
+            </form>
+            {/*    Balances of Link */}
+            {data.length > 0 && (
+              <div>
+                {/*    Balances of all ERC2 */}
+                <p>
+                  Balances of all ERC20 tokens number of Ethereum:
+                  <span className="fs-5 ms-2 fw-bold">
+                    {data.length > 0 && data[0].totalSupply}
+                  </span>
+                </p>
+                {/*    Balance of Ethereum */}
+                <p>
+                  Balance of Ethereum native tokens:
+                  <span className="fs-5 ms-2 fw-bold">
+                    {data.length > 0 && data[0].balance}
+                  </span>
+                </p>
+                {/*  How many tokens */}
+                <p>
+                  How many tokens is on given address:
+                  <span className="fs-5 ms-2 fw-bold">
+                    {data.length > 0 && data[0].tokenCount}
+                  </span>
+                </p>
+
+                {/*   List of tokens */}
+                <p className="overflow__main">
+                  How many tokens is on given address:
+                  <span className="fs-5 ms-2 fw-bold">
+                    {data.length > 0 && JSON.stringify(data[0].allTokens)}
+                  </span>
+                </p>
+              </div>
+            )}
+            {/* download */}
+            {download === true && <div className="text-info">loading ...</div>}
+            {error.length > 0 && (
+              <div className="alert alert-danger small overflow__main">
+                {error}
+              </div>
+            )}
+            <div>
+              <hr />
+              {/* file */}
+              {secondData.length > 0 && (
+                /*     link */
+                <div>
+                  <p className="alert alert-success small">
+                    Link on the file with interval:
+                    <a
+                      className="ms-2"
+                      target={"_blank"}
+                      rel="noreferrer"
+                      href={`${process.env.REACT_APP_URL_SERVER}config.json`}
+                    >
+                      Link
+                    </a>
+                  </p>
+                {/*   All dat */}
+                  <p className="word-break overflow__main">
+                    <h4>All data</h4>
+                    JSON.stringify(secondData)
+                  </p>
+                </div>
+              )}
+              {downloadAllTokens === true && (
+                <div className="text-info">loading ...</div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
